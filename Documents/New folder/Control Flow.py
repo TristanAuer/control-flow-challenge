c44ea6f2a46c737839995508ffabb3d4d678668f
Auer <tristaue@amazon.com>
2022-05-26 08:11:11 -0400

    added challenge

--- Documents/New folder/Control Flow.py
+++ Documents/New folder/Control Flow.py
@@ -76,3 +76,24 @@
 # # should print True
 # print(divisible_by_ten(25))
 # # should print False
+
+#   next challenge
+
+# Create a function named not_sum_to_ten() that has two parameters named num1 and num2.
+
+# Return True if num1 and num2 do not sum to 10. Return False otherwise.
+# Hint Remember that in order to test if two values are not equal we use !=.
+
+# Write your not_sum_to_ten function here:
+def not_sum_to_ten(num1, num2):
+    if (num1 + num2) != 10:
+        return True
+    else:
+        return False
+# Uncomment these function calls to test your not_sum_to_ten function:
+print(not_sum_to_ten(9, -1))
+# should print True
+print(not_sum_to_ten(9, 1))
+# should print False
+print(not_sum_to_ten(5,5))
+# should print False
