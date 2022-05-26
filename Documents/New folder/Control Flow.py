2bfc3e4d693a5247e1a6b2e27d5205fecd68e111
Auer <tristaue@amazon.com>
2022-05-26 08:34:05 -0400

    contradiction

--- Documents/New folder/Control Flow.py
+++ Documents/New folder/Control Flow.py
@@ -137,3 +137,23 @@
 # # should print False
 
 #   next challenge
+
+# Create a function named always_false() that has one parameter named num.
+# Using an if statement, your variable num, and the operators >, and <, make it so your function will return False no matter what number is stored in num.
+
+# An if statement that is always false is called a contradiction. You will rarely want to do this while programming, but it is important to realize it is possible to do this.
+# Hint Try to think of an example of a condition which is always false. For example, a number cannot be greater than and less than itself at the same time.
+
+# Write your always_false function here:
+def always_false(num):
+    if 1 < num < 1:
+        return True
+    else:
+        return False
+# Uncomment these function calls to test your always_false function:
+print(always_false(0))
+# should print False
+print(always_false(-1))
+# should print False
+print(always_false(1))
+# should print False
