0f12d511062f0b71931354d79645074ced21c77a
Auer <tristaue@amazon.com>
2022-05-26 10:30:14 -0400

    return strings

--- Documents/New folder/Control Flow.py
+++ Documents/New folder/Control Flow.py
@@ -144,16 +144,39 @@
 # An if statement that is always false is called a contradiction. You will rarely want to do this while programming, but it is important to realize it is possible to do this.
 # Hint Try to think of an example of a condition which is always false. For example, a number cannot be greater than and less than itself at the same time.
 
-# Write your always_false function here:
-def always_false(num):
-    if 1 < num < 1:
-        return True
-    else:
-        return False
-# Uncomment these function calls to test your always_false function:
-print(always_false(0))
-# should print False
-print(always_false(-1))
-# should print False
-print(always_false(1))
-# should print False
+# Write your always_false function here: commented out line 148 to 159 uncomment to run code.
+# def always_false(num):
+#     if 1 < num < 1:
+#         return True
+#     else:
+#         return False
+# # Uncomment these function calls to test your always_false function:
+# print(always_false(0))
+# # should print False
+# print(always_false(-1))
+# # should print False
+# print(always_false(1))
+# # should print False
+
+#   next challenge
+
+# Create a function named movie_review() that has one parameter named rating.
+
+# If rating is less than or equal to 5, return "Avoid at all costs!". If rating is between 5 and 9, return "This one was fun.". If rating is 9 or above, return "Outstanding!"
+# Hint Use a series of if statements to test the different ranges. We can check each condition separately to determine which string to return.
+
+# Write your movie_review function here:
+def movie_review(rating):
+    if rating <= 5:
+        return "Avoid at all costs!"
+    elif 5 < rating < 9:
+        return "This one was fun."
+    elif rating >= 9:
+        return "Outstanding!"
+# Uncomment these function calls to test your movie_review function:
+print(movie_review(9))
+# should print "Outstanding!"
+print(movie_review(4))
+# should print "Avoid at all costs!"
+print(movie_review(6))
+# should print "This one was fun."
